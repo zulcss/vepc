8a3c6fe4d911067f79ebad17b81e5b89ca4aad6b
Chuck Short <charles.short@ericsson.com>
2017-07-15 21:56:51 -0500

    Fix vpec.conf path
    Signed-off-by: Chuck Short <charles.short@ericsson.com>

--- dump.py
+++ dump.py
@@ -3,7 +3,7 @@
 import json
 import xmltodict
 
-with open('vepc.conf', 'rv') as f:
+with open('/root/vepc.conf', 'rv') as f:
     d = xmltodict.parse(f, xml_attribs=True)
     data = json.dumps(d, indent=4)
 print data
